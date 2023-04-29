8bc0fe44af4668d07b7dbd8bbb24adcfa7cf3757
Kirill Novozhilov <kirk@nsvision.com>
2023-04-29 08:36:56 +0400

    hotfix include.php

--- bx.base/include.php
+++ bx.base/include.php
@@ -1,10 +1,12 @@
 <?php
 
+use Arrilot\BitrixBlade\BladeProvider;
 use Bitrix\Main\Application;
 use Symfony\Component\Dotenv\Dotenv;
 
 if (file_exists(Application::getDocumentRoot() . '/local/vendor/autoload.php')) {
     include Application::getDocumentRoot() . '/local/vendor/autoload.php';
+    BladeProvider::register();
 }
 
 if (file_exists(Application::getDocumentRoot() . '/local/.env')) {
